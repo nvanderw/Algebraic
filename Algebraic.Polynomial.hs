89012d9fdc6defd098a3b42371e34a476b846661
Nick Vanderweit <nicholas.vanderweit@colorado.edu>
2012-05-03 19:43:09 -0600

    Added Ring instance for (Ring r) => Polynomial r. Multiplication now works.

--- Algebraic.Polynomial.hs
+++ Algebraic.Polynomial.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Algebraic.Polynomial (
-    Polynomial) where
+    Polynomial,
+    degree) where
 
 import Algebraic
+import Data.Array
 
 -- |Ring of polynomials with coefficients in the ring r
 data Polynomial r = Polynomial [r]
@@ -22,3 +24,37 @@ instance GroupPlus g => GroupPlus (Polynomial g) where
     gzero = Polynomial []
 
     gneg (Polynomial xs) = Polynomial $ map gneg xs
+
+-- |Yields the degree of a nonzero polynomial, or Nothing if the polynomial
+-- is zero
+degree :: Polynomial a -> Maybe Int
+degree (Polynomial []) = Nothing
+degree (Polynomial xs) = Just (length xs)
+
+instance (Ring r) => Ring (Polynomial r) where
+    -- Multiplying by zero should always yield zero
+    (Polynomial []) >*< _ = gzero
+    _ >*< (Polynomial []) = gzero
+
+    (Polynomial xs) >*< (Polynomial ys) = Polynomial rs
+      where
+        -- Degree of each input polynomial
+        deg1 = length xs - 1
+        deg2 = length ys - 1
+
+        -- Maximum degree of resulting polynomial
+        resultDeg = deg1 + deg2
+
+        -- Infinite list of zeros to pad the end of the polynomial
+        zeros = repeat gzero :: [r]
+
+        -- Pad ends of polynomials up to resulting degree and convert to
+        -- arrays
+        arr1 = listArray (0, resultDeg) (xs ++ zeros)
+        arr2 = listArray (0, resultDeg) (ys ++ zeros)
+
+        -- Function to get the nth coefficient of the resulting polynomial
+        getCoeff n = foldl (>+<) gzero $ map (\i -> (arr1 ! i) >*< (arr2 ! (n - i))) [0..n]
+
+        -- Coefficients of resulting polynomial
+        rs = truncateCoeffs $ map getCoeff [0..(resultDeg)]
